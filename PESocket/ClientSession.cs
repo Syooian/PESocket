aa1985877cf2b291548a0c97963b638d826c8b86
wei_an <wei_an@phalanity.com.tw>
2023-05-09 10:44:59 +0800

    自定義的串接項目

---
+++ PESocket/ClientSession.cs
@@ -0,0 +1,49 @@
+﻿using PENet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PENet
+{
+    public class ClientSession : PESession<NetMsg>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        protected override void OnConnected()
+        {
+            Console.WriteLine("已連線至Server");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Msg"></param>
+        protected override void OnReciveMsg(NetMsg Msg)
+        {
+            base.OnReciveMsg(Msg);
+
+            Console.WriteLine("接收訊息 Code : " + Msg.Code + ", Text : " + Msg.Text + ", Volume : " + Msg.Volume + ", Operation : " + Msg.Operation);
+        }
+
+        protected override void OnDisConnected()
+        {
+            Console.WriteLine("連線已中斷");
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum OpCode
+    {
+        None = 0,
+        PlayVideo = 1,
+        PlayVolume = 2,
+        PPTOperation = 3,
+        Mechanical = 4,
+        Dmx = 5,
+        Availed = 6
+    }
+}
---
+++ PESocket/NetMsg.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PENet;
+
+namespace PENet
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Serializable]
+    public class NetMsg : PEMsg
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public OpCode Code;
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Text;
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Volume;
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Operation;
+    }
+}
